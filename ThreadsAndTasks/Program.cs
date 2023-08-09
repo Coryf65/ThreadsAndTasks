52acf0a4f4470b5cfb4c3dd61dbc9da5c12a0229
Cory Fabian <Coryf65@users.noreply.github.com>
2023-08-08 19:48:11 -0500

    initial commit

---
+++ ThreadsAndTasks/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Checking out threading and tasks");
+
+Task task1 = new(() => Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId}."));
+task1.Start();
+task1.Wait()
